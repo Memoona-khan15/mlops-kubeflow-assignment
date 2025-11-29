0711b3731b7024f9b2064aed1ccf27ea4d0b4b19
Memoona-khan15 <i220490@nu.edu.pk>
2025-11-29 16:26:54 +0500

    Task 4 - MLflow model registry + CI pipeline

--- mlflow_pipeline.py
+++ mlflow_pipeline.py
@@ -1,24 +1,23 @@
-from src.extract_data import extract_data
+import pandas as pd
+import mlflow
 from src.preprocess_data import preprocess_data
 from src.train_model import train_model
-from src.evaluate_model import evaluate_model
+from src.download_data import create_dataset
 
 def run_pipeline():
     print("STEP 1: Extracting data...")
-    df = extract_data()
+    create_dataset()
+
+    df = pd.read_csv("data/raw_data.csv")
 
     print("STEP 2: Preprocessing...")
     X_train, X_test, y_train, y_test = preprocess_data(df)
 
     print("STEP 3: Training...")
-    model = train_model(X_train, y_train)
+    accuracy = train_model(X_train, y_train, X_test, y_test)
 
-    print("STEP 4: Evaluating...")
-    mse, r2 = evaluate_model(model, X_test, y_test)
+    print("STEP 4: Completed. Accuracy:", accuracy)
 
-    print("\n🎉 PIPELINE COMPLETED SUCCESSFULLY 🎉")
-    print("💡 MSE:", mse)
-    print("💡 R2 Score:", r2)
 
 if __name__ == "__main__":
     run_pipeline()
--- src/train_model.py
+++ src/train_model.py
@@ -1,19 +1,34 @@
 import mlflow
 import mlflow.sklearn
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 import joblib
 import os
 
-def train_model(X_train, y_train):
+def train_model(X_train, y_train, X_test, y_test):
     mlflow.set_experiment("MLflow-Pipeline")
-    with mlflow.start_run(run_name="train_model"):
-        mlflow.sklearn.autolog()
 
-        model = RandomForestRegressor(n_estimators=100, random_state=42)
+    with mlflow.start_run(run_name="train_model"):
+
+        model = DecisionTreeClassifier(max_depth=5, random_state=42)
         model.fit(X_train, y_train)
 
-        os.makedirs("artifacts", exist_ok=True)
-        joblib.dump(model, "artifacts/model.pkl")
-        mlflow.log_artifact("artifacts/model.pkl")
+        preds = model.predict(X_test)
+        acc = accuracy_score(y_test, preds)
 
-        return model
+        # Log metric
+        mlflow.log_metric("accuracy", acc)
+
+        # Save model locally
+        os.makedirs("artifacts", exist_ok=True)
+        model_path = "artifacts/model.pkl"
+        joblib.dump(model, model_path)
+
+        # Log artifact
+        mlflow.log_artifact(model_path)
+
+        # Register model in MLflow Model Registry
+        mlflow.sklearn.log_model(model, artifact_path="model", registered_model_name="BreastCancerModel")
+
+        print(f"Model trained. Accuracy: {acc}")
+        return acc
